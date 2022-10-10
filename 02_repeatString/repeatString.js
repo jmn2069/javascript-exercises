324728501aacaf4060fa96947385aa0a644379dd
Jonathan <gdk2012@gmail.com>
2022-10-10 12:02:07 -0700

    debug exercises 1 & 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, repeat) {
+    let word = '';
+    for (let i = 0; i < repeat; i++) {
+        word += string;
+    }
+    if (repeat === -1) {
+        word += 'ERROR';
+    }
+    return word
 };
 
 // Do not edit below this line
