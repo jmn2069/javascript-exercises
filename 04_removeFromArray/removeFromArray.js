babf0eb20a41bb369d6698a4019f5d9a19661975
Jonathan <gdk2012@gmail.com>
2022-10-10 13:10:16 -0700

    debug exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
 const removeFromArray = function() {
+    let array = arguments[0];
 
+    for (let i = 1; i < arguments.length + 1; i++) {
+        for (let j = 0; j < array.length; j++) {
+            if (array[j] === arguments[i]) {
+                array.splice(j, 1);
+            }
+        }
+    }
+
+    return array;
 };
 
 // Do not edit below this line
