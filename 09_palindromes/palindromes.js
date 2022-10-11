9508d1920e5d62353a2339f0a3143d527e37cfe8
Jonathan <gdk2012@gmail.com>
2022-10-10 23:16:45 -0700

    complete palindromes function for test

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let regex = /[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~ ]/g;
+    let string2 = string.toLowerCase().replace(regex, '');
+    let reverseString = string2.split('').reverse().join('');
+    return reverseString === string2;
 };
 
 // Do not edit below this line
