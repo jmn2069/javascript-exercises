e050e7427583bc6da6217976f01973324e2f4420
Jonathan <gdk2012@gmail.com>
2022-10-10 23:38:14 -0700

    complete fibonacci function for test

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if (num < 0){
+        return "OOPS";
+    }
+    let array = [1, 1];
+    for (let i = 0; i < num; i++) {
+        array.push(array[i] + array[i + 1]);
+    }
+    return array[num - 1];
 };
 
 // Do not edit below this line
