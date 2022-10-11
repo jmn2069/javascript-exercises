b364bcdf72b691c0fb2cc6e02f39a2fe8ca89cdd
Jonathan <gdk2012@gmail.com>
2022-10-11 12:55:10 -0700

    build caesar function for test

--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -1,5 +1,39 @@
-const caesar = function() {
-
+const caesar = function(string, key) {
+    let caesarString = '';
+    while (key > 26) {
+        key -= 26;
+    }
+    while (key < 0) {
+        key += 26;
+    }
+    Loop1:
+    for (let i = 0; i < string.length; i++) {
+        if (string.charCodeAt(i) > 64 && string.charCodeAt(i) < 91) { // is upper case
+            if (string.charCodeAt(i) + key < 65) {
+                caesarString += String.fromCharCode(90 - (64 - (string.charCodeAt(i) + key)));
+                continue Loop1;
+            } else if (string.charCodeAt(i) + key > 90) {
+                caesarString += String.fromCharCode(((string.charCodeAt(i) + key) - 26));
+                continue Loop1;
+            }
+            caesarString += String.fromCharCode(string.charCodeAt(i) + key);
+            continue Loop1;
+        }
+        if (string.charCodeAt(i) > 96 && string.charCodeAt(i) < 123) { // is lower case
+            if (string.charCodeAt(i) + key < 97) {
+                caesarString += String.fromCharCode(122 - (97 - (string.charCodeAt(i) + key)));
+                continue Loop1;
+            }
+            if (string.charCodeAt(i) + key > 122) {
+                caesarString += String.fromCharCode(((string.charCodeAt(i) + key) - 26));
+                continue Loop1;
+            }
+            caesarString += String.fromCharCode(string.charCodeAt(i) + key);
+            continue Loop1;
+        }
+    caesarString += string[i];
+    }
+    return caesarString;
 };
 
 // Do not edit below this line
