cf8fedfb64e9f38af21bafadd1905f835a31aa57
Jonathan <gdk2012@gmail.com>
2022-10-10 22:05:01 -0700

    build calculator functions for test

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  let num3 = 0;
+  for (let i = 0; i < array.length; i++) {
+    num3 += array[i];
+  }
+  return num3;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let num3 = 1;
+  for (let i = 0; i < array.length; i++) {
+    num3 *= array[i];
+  }
+  return num3;
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+  return (num1 ** num2)
 };
 
-const factorial = function() {
-	
+const factorial = function(num1) {
+	let num3 = 1;
+  for (let i = 1; i < num1 + 1; i++){
+    num3 *= i;
+  }
+  return num3;
 };
 
 // Do not edit below this line
