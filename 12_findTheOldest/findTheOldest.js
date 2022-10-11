0aa053eda4486b33a5c80a6a348db702c94f92d5
Jonathan <gdk2012@gmail.com>
2022-10-11 10:10:13 -0700

    build findTheOldest function for test

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,21 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    let date = new Date().getFullYear();
+    let oldestAge = 0;
+    let oldestPerson = [];
+    for (let i = 0; i < people.length; i++) {
+        if (people[i].yearOfDeath === undefined) {
+            if(oldestAge < (date - people[i].yearOfBirth)) {
+                oldestAge = (date - people[i].yearOfBirth)
+                oldestPerson = people[i];
+                continue;
+            }
+        }
+        if (oldestAge < (people[i].yearOfDeath - people[i].yearOfBirth)) {
+            oldestAge = (people[i].yearOfDeath - people[i].yearOfBirth);
+            oldestPerson = people[i];
+        }
+    }
+    return oldestPerson;
 };
 
 // Do not edit below this line
