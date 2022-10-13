d77dc95a5c98975ccbf79b7cd4384c8818d972f2
Jonathan <gdk2012@gmail.com>
2022-10-13 13:51:50 -0700

    remove old code

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,9 +1,5 @@
 const palindromes = function (string) {
     let regex = /[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~ ]/g;
-    // let string2 = string.toLowerCase().replace(regex, '');
-    // let reverseString = string2.split('').reverse().join('');
-    // return reverseString === string2;
-
     return string.toLowerCase().replace(regex, '') === string.toLowerCase().replace(regex, '').split('').reverse().join('')
 };
 
