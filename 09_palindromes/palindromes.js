8d29a9b9818fb1cd0dca537e61beae66374ac247
Jonathan <gdk2012@gmail.com>
2022-10-13 13:49:44 -0700

    refactor function code

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,8 +1,10 @@
 const palindromes = function (string) {
     let regex = /[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~ ]/g;
-    let string2 = string.toLowerCase().replace(regex, '');
-    let reverseString = string2.split('').reverse().join('');
-    return reverseString === string2;
+    // let string2 = string.toLowerCase().replace(regex, '');
+    // let reverseString = string2.split('').reverse().join('');
+    // return reverseString === string2;
+
+    return string.toLowerCase().replace(regex, '') === string.toLowerCase().replace(regex, '').split('').reverse().join('')
 };
 
 // Do not edit below this line
